c79db8846663fd2694ae1657f817c8b8672ee7ba
Xuesong Hu <xuesong.hu@ibm.com>
2021-05-01 13:58:06 -0700

    implement binary search as a commonJS module

---
+++ src/binarySearch.test.js
@@ -0,0 +1,8 @@
+const binarySearch = require('./binarySearch');
+
+describe("binary search", () => {
+    it("should pass all test cases", () => {
+        const ret = binarySearch([1,2,3,4], 3);
+        expect(ret).toEqual(2);
+    });
+});

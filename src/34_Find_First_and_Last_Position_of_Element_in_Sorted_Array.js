1d44a2b1a88d7317db07a2b74022bcffcde34c0c
Xuesong Hu <xuesong.hu@ibm.com>
2021-05-15 13:39:58 -0700

    implement an alt approach

--- src/34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.js
+++ src/34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.js
@@ -28,8 +28,42 @@ function findIndexOneSide(nums, target, isLeft) {
   return -1;
 }
 
+function binarySearch(nums, left, right, target) {
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
+}
+
 export default function searchRange(nums, target) {
   const lidx = findIndexOneSide(nums, target, true);
   const ridx = findIndexOneSide(nums, target, false);
   return [lidx, ridx];
+}
+
+export function searchRangeAlt(nums, target) {
+  const firstPos = binarySearch(nums, 0, nums.length - 1, target);
+  if (firstPos === -1) return [-1, -1];
+  let startPos = firstPos,
+    endPos = firstPos,
+    temp1,
+    temp2;
+  while (startPos !== -1) {
+    temp1 = startPos;
+    startPos = binarySearch(nums, 0, startPos - 1, target);
+  }
+  startPos = temp1;
+  while (endPos !== -1) {
+    temp2 = endPos;
+    endPos = binarySearch(nums, endPos + 1, nums.length - 1, target);
+  }
+  endPos = temp2;
+  return [startPos, endPos];
 }

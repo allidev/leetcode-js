92825d9d2f89b11b79e2bdb2f7d0a964449f66f5
Xuesong Hu <xuesong.hu@ibm.com>
2021-05-15 09:39:50 -0700

    adding two problems

---
+++ src/sum-to-target.js
@@ -0,0 +1,24 @@
+function recursive(N, shouldInclude) {
+    // base case
+    if (N === 0) {
+        return [];
+    }
+    // recursive case
+    const ans = [];
+    for (let i = 1; i <= N; i++) {
+        const temp = recursive(N-i, true);
+        const size = temp.length;
+        if (size > 0) {
+            for (let j = 0; j < size; j++) {
+                ans.push(""+i+'+'+temp[j]);
+            }
+        } else if (shouldInclude) {
+            ans.push(""+i);
+        }
+    }
+    return ans;
+}
+
+export default function sumToTarget(N) {
+    return recursive(N, false);
+}
---
+++ src/sum-to-target.test.js
@@ -0,0 +1,8 @@
+import sumToTarget from './sum-to-target';
+
+describe('Sum to target', () => {
+    it('should pass the test', () => {
+        const ret = sumToTarget(4);
+        console.log(ret);
+    });
+});

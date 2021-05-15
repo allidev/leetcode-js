2f765375824ba1df2dabdb3fc937cb1d2d56e11f
Xuesong Hu <xuesong.hu@ibm.com>
2021-05-15 10:03:41 -0700

    adding comments

--- src/sum-to-target.test.js
+++ src/sum-to-target.test.js
@@ -1,5 +1,15 @@
 import sumToTarget from './sum-to-target';
 
+// 1 + Recursive(3) ===>
+//   1 + Recursive(3)[0]
+//   1 + Recursive(3)[1]
+//   1 + Recursive(3)[2]
+//   1 + Recursive(3)[3]
+// 2 + Recursive(2)
+// 3 + Recursive(1)
+// 4 + Recursive(0)
+
+
 describe('Sum to target', () => {
     it('should pass the test', () => {
         const ret = sumToTarget(4);

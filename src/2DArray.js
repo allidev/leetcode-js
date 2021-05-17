0a6c26ce00ab5c5f924fe9f7449328b49c03e314
Xuesong Hu <xuesong.hu@ibm.com>
2021-05-16 20:32:13 -0700

    implement bfs

--- src/2DArray.js
+++ src/2DArray.js
@@ -22,6 +22,30 @@ function dfs(matrix, row, col, seen, values) {
   }
 }
 
+function bfs(matrix, seen, values) {
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+  const queue = [];
+  queue.push([2, 2]);
+  seen[2][2] = true;
+  while (queue.length > 0) {
+    const n = queue.length;
+    for (let i = 0; i < n; i++) {
+      const [row, col] = queue.shift();
+      values.push(matrix[row][col]);
+      for (const dir of directions) {
+        const nextRow = row + dir[0];
+        const nextCol = col + dir[1];
+        if (nextRow < 0 || nextRow >= rows) continue;
+        if (nextCol < 0 || nextCol >= cols) continue;
+        if (seen[nextRow][nextCol]) continue;
+        queue.push([nextRow, nextCol]);
+        seen[nextRow][nextCol] = true;
+      }
+    }
+  }
+}
+
 function dfs_post_order(matrix, row, col, seen, values) {
   // base case
   const rows = matrix.length;
@@ -54,5 +78,14 @@ export function traversalDFSPO(matrix) {
     .map(() => new Array(matrix[0].length).fill(false));
   const values = [];
   dfs_post_order(matrix, 0, 0, seen, values);
+  return values;
+}
+
+export function traversalBFS(matrix) {
+  const seen = new Array(matrix.length)
+    .fill(0)
+    .map(() => new Array(matrix[0].length).fill(false));
+  const values = [];
+  bfs(matrix, seen, values);
   return values;
 }
--- src/2DArray.test.js
+++ src/2DArray.test.js
@@ -1,4 +1,4 @@
-import traversalDFS, { traversalDFSPO } from "./2DArray";
+import traversalDFS, { traversalDFSPO, traversalBFS } from "./2DArray";
 
 describe("2D Array traversal", () => {
   it("should pass all test cases", () => {
@@ -24,6 +24,19 @@ describe("2D Array traversal", () => {
     const res = traversalDFSPO(matrix);
     const expected = [
       16, 11, 6, 7, 12, 17, 18, 13, 8, 9, 14, 19, 20, 15, 10, 5, 4, 3, 2, 1,
+    ];
+    expect(res).toEqual(expected);
+  });
+  it("should pass all test cases", () => {
+    const matrix = [
+      [1, 2, 3, 4, 5],
+      [6, 7, 8, 9, 10],
+      [11, 12, 13, 14, 15],
+      [16, 17, 18, 19, 20],
+    ];
+    const res = traversalBFS(matrix);
+    const expected = [
+      13, 8, 14, 18, 12, 3, 9, 7, 15, 19, 17, 11, 4, 2, 10, 6, 20, 16, 5, 1,
     ];
     expect(res).toEqual(expected);
   });

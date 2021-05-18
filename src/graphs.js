ee9b13a9d7997927ea9a8e7c3f8eb9c2630ac710
Xuesong Hu <xuesong.hu@ibm.com>
2021-05-17 22:50:17 -0700

    implement dfs

--- src/graphs.js
+++ src/graphs.js
@@ -14,3 +14,15 @@ export function bfs(adjList) {
   }
   return ans;
 }
+
+export function dfs(adjList, vertex, visited, values) {
+  // base case
+  if (visited[vertex]) return;
+  // recursive case
+  values.push(vertex);
+  visited[vertex] = true;
+  const edges = adjList[vertex];
+  for (const edge of edges) {
+    if (!visited[edge]) dfs(adjList, edge, visited, values);
+  }
+}
--- src/graphs.test.js
+++ src/graphs.test.js
@@ -1,4 +1,4 @@
-import { bfs } from "./graphs";
+import { bfs, dfs } from "./graphs";
 
 describe("graphs", () => {
   it("should pass all test cases", () => {
@@ -15,5 +15,22 @@ describe("graphs", () => {
     ];
     const res = bfs(adjList);
     expect(res).toEqual([0, 1, 3, 4, 5, 2, 6, 8, 7]);
+  });
+  it("should pass all test cases", () => {
+    const adjList = [
+      [1, 3],
+      [0],
+      [3, 8],
+      [0, 4, 5, 2],
+      [3, 6],
+      [3],
+      [4, 7],
+      [6],
+      [2],
+    ];
+    const visited = {};
+    const values = [];
+    dfs(adjList, 0, visited, values);
+    expect(values).toEqual([0, 1, 3, 4, 6, 7, 5, 2, 8]);
   });
 });

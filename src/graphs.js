74987e4090a41644e88ec8937291e7dfbc81669b
Xuesong Hu <xuesong.hu@ibm.com>
2021-05-17 22:36:16 -0700

    use object type

--- src/graphs.js
+++ src/graphs.js
@@ -1,17 +1,17 @@
 export function bfs(adjList) {
   const ans = [];
   const queue = [];
-  const visited = new Map();
+  const visited = {};
   queue.push(0);
-  visited.set(0, true);
+  visited[0] = true;
   while (queue.length > 0) {
     const pos = queue.shift();
     ans.push(pos);
     const edges = adjList[pos];
     for (const edge of edges) {
-      if (visited.get(edge)) continue;
+      if (visited[edge]) continue;
       queue.push(edge);
-      visited.set(edge, true);
+      visited[edge] = true;
     }
   }
   return ans;

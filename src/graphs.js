2f318fde475534f959597b8817afa1c1ffc96cd0
Xuesong Hu <xuesong.hu@ibm.com>
2021-05-17 22:38:54 -0700

    set visited only once

--- src/graphs.js
+++ src/graphs.js
@@ -3,15 +3,14 @@ export function bfs(adjList) {
   const queue = [];
   const visited = {};
   queue.push(0);
-  visited[0] = true;
   while (queue.length > 0) {
     const pos = queue.shift();
+    visited[pos] = true;
     ans.push(pos);
     const edges = adjList[pos];
     for (const edge of edges) {
       if (visited[edge]) continue;
       queue.push(edge);
-      visited[edge] = true;
     }
   }
   return ans;
